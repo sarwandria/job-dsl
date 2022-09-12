81049b3946d29464a1f34db38ff389f4fc9d036e
sarwandria <sarwandria.f@gmail.com>
2022-09-12 17:10:14 +0700

    fix identation

--- java-maven.groovy
+++ java-maven.groovy
@@ -31,13 +31,12 @@ pipeline {
                         }
                     }
                 }
+                stage('Deliver') {
+                    steps {
+                        sh './jenkins/scripts/deliver.sh'
+                    }
+                }
             }
         }    
-        stage('Deliver') {
-            steps {
-                sh './jenkins/scripts/deliver.sh'
-            }
-        }
-        
     }
 }
